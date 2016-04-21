900cfd138c3cffbb6b7a77fcdbcaa920318a8019
Frankie <fdsimms@gmail.com>
2016-04-21 14:11:23 -0400

    Add stackCtrl, begin making html form for adding a timer.

--- js/timestack.js
+++ js/timestack.js
@@ -1,1 +1,9 @@
 var timestack = angular.module('timestack', []);
+
+timestack.controller('stackCtrl', function ($scope) {
+  $scope.timers = [{timeLeft: 100 }];
+
+  $scope.addTimer = function (timeLeft) {
+    $scope.timers.push({ timeLeft: timeLeft });
+  }
+})
