d3e3dd3d9cd127cfa49421382a62296710df8a40
Frankie <fdsimms@gmail.com>
2016-04-21 17:40:47 -0400

    Reconfigure stack.startTimer to play well with multiple timers.

--- js/timestack.js
+++ js/timestack.js
@@ -5,18 +5,24 @@ timestack.controller('stackCtrl', function ($interval) {
 
   stack.formSeconds = 0;
   stack.timers = [];
+  stack.timersRunning = false;
 
   stack.addTimer = function () {
     stack.timers.push({ timeLeft: stack.formSeconds });
   };
 
   stack.startTimer = function () {
-    var timers = $interval(function () {
-      if (stack.timers[0].timeLeft > 0) {
-        stack.timers[0].timeLeft -= 1;
-      } else {
-        $interval.cancel(timers);
-      }
-    }, 1000);
+    if (stack.timers.length > 0) {
+      stack.timersRunning = true;
+      var timers = $interval(function () {
+        if (stack.timers[0].timeLeft > 0) {
+          stack.timers[0].timeLeft -= 1;
+        } else {
+          $interval.cancel(timers);
+          stack.timers = stack.timers.slice(1);
+          stack.startTimer();
+        }
+      }, 1000);
+    }
   };
 })
