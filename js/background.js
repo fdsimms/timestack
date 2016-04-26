98e43fd4fc5145ef9728cd29fd4a0bf62b3e3422
Frankie <fdsimms@gmail.com>
2016-04-26 19:10:35 -0400

    Move timer logic to background page script.

--- js/background.js
+++ js/background.js
@@ -4,8 +4,22 @@ var cookie = {
   isPaused: false
 };
 
+var timerInt;
+
 function timers() {
   return cookie.timers;
+};
+
+function timersRunning() {
+  return cookie.timersRunning;
+};
+
+function isPaused() {
+  return cookie.isPaused;
+};
+
+function stackIsEmpty() {
+  return timers().length <= 0;
 };
 
 function pushToTimers(val) {
@@ -16,8 +30,57 @@ function getCookie() {
   return cookie;
 }
 
-function startTimer() {
-  setInterval(function () {
-    cookie.timers[0].timeLeft--;
-  }, 1000)
+function tick() {
+  cookie.timers[0].timeLeft--;
 }
+
+function startTimer() {
+  if (!stackIsEmpty()) {
+    cookie.timersRunning = true;
+    cookie.isPaused = false;
+    setTimerInt();
+  }
+}
+
+function resetTimer(idx) {
+  var timer = timers()[idx];
+  timer.timeLeft = timer.timeInSeconds;
+};
+
+function pauseTimers() {
+  stopInterval();
+  cookie.isPaused = true;
+}
+
+function setTimerInt () {
+  timerInt = setInterval(function () {
+    if (timers()[0].timeLeft > 0) {
+      tick();
+    } else {
+      endFirstTimerAndContinue();
+    }
+  }, 1000);
+};
+
+function endFirstTimerAndContinue () {
+  stopInterval();
+  removeTimer(0);
+  if (stackIsEmpty()) {
+    cookie.timersRunning = false;
+  } else {
+    startTimerInt();
+  }
+};
+
+function stopInterval() {
+  clearInterval(timerInt);
+};
+
+function removeTimer(idx) {
+  cookie.timers.splice(idx, 1);
+  if (stackIsEmpty()) {
+    stopInterval();
+    cookie.timersRunning = false;
+    cookie.isPaused = false;
+  };
+};
--- js/timestack.js
+++ js/timestack.js
@@ -15,30 +15,14 @@ timestack.controller('stackCtrl', function ($scope, $interval) {
     })
   };
 
-  stack.updateCookie = function () {
+  stack.updateInfo = function () {
     getBGP(function (bgp) {
       $scope.$apply(function () {
         stack.timers = bgp.timers();
+        stack.timersRunning = bgp.timersRunning();
+        stack.isPaused = bgp.isPaused();
       });
     });
-  };
-
-  stack.getCookieItem = function (cookieName) {
-    var cookie = stack.cookie();
-    return !!cookie ? cookie[cookieName] : null;
-  }
-
-  stack.setCookieItem = function (cookieName, val) {
-    var cookie = stack.cookie();
-    cookie[cookieName] = val;
-  }
-
-  stack.removeTimerFromCookie = function (idx) {
-
-  };
-
-  stack.isEmpty = function () {
-    return stack.timers.length <= 0;
   };
 
   stack.pushTimerToStack = function (timer) {
@@ -70,62 +54,19 @@ timestack.controller('stackCtrl', function ($scope, $interval) {
     stack.resetForm();
   };
 
-
   stack.startTimer = function () {
-    if (!stack.isEmpty()) {
-      var storage = window.localStorage;
-      chrome.runtime.getBackgroundPage(function (bgp) {
-        bgp.startTimer(stack.cookie(), storage)
-      });
-      stack.timersRunning = true;
-      stack.setCookieItem('timersRunning', true);
-      stack.setCookieItem('isPaused', false);
-      stack.isPaused = false;
-      stack.setTimerInt();
-    }
-  };
-
-  stack.setTimerInt = function () {
-    stack.timerInt = $interval(function () {
-      if (stack.timers[0].timeLeft > 0) {
-        stack.tick();
-      } else {
-        stack.endFirstTimerAndContinue();
-      }
-    }, 1000);
+    getBGP(function (bgp) {
+      bgp.startTimer();
+      stack.timerInt = setInterval(function () {
+        stack.updateInfo();
+      }, 1000);
+    });
   };
 
   stack.pauseTimers = function () {
-    stack.stopInterval();
-    stack.isPaused = true;
-    stack.setCookieItem('isPaused', true);
-  };
-
-  stack.tick = function () {
-    $interval(function () {
-      chrome.runtime.getBackgroundPage(function (bgp) {
-        stack.cookies = bgp.cookie();
-      });
-    }, 1000)
-  };
-
-  stack.bgpTick = function () {
-
-  };
-
-  stack.stopInterval = function () {
-    $interval.cancel(stack.timerInt);
-  };
-
-  stack.endFirstTimerAndContinue = function () {
-    stack.stopInterval();
-    stack.removeTimer(0);
-    if (stack.isEmpty()) {
-      stack.timersRunning = false;
-      stack.setCookieItem('timersRunning', false);
-    } else {
-      stack.startTimer();
-    }
+    getBGP(function (bgp) {
+      bgp.pauseTimers();
+    });
   };
 
   stack.moveTimerUp = function (idx) {
@@ -149,26 +90,14 @@ timestack.controller('stackCtrl', function ($scope, $interval) {
   };
 
   stack.resetTimer = function (idx) {
-    var timer = stack.timers[idx];
-    timer.timeLeft = timer.timeInSeconds;
-    stack.setCookieItem('timers', stack.timers);
-  };
-
-  stack.removeTimer = function (idx) {
-    stack.timers.splice(idx, 1);
-    stack.removeTimerFromCookie(idx);
-    if (stack.isEmpty()) {
-      stack.stopInterval();
-      stack.timersRunning = false;
-      stack.isPaused = false;
-      stack.setCookieItem('timersRunning', false);
-      stack.setCookieItem('isPaused', false);
-    };
+    getBGP(function (bgp) {
+      bgp.resetTimer(idx);
+    });
   };
 
   stack.formSeconds = 0;
   stack.formMinutes = 0;
   stack.formHours = 0;
   stack.timerDesc = "";
-  stack.updateCookie();
+  stack.updateInfo();
 })
