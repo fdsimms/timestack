d2b50654fdaeb3d2b51f8fd17764ed171b981541
Frankie <fdsimms@gmail.com>
2016-04-28 16:31:28 -0400

    Create iconService.js.

---
+++ js/services/iconService.js
@@ -0,0 +1,6 @@
+angular.module("timestack.services.iconService", [])
+  .service("iconService", function () {
+    var iconService = this;
+    bgpService.getBGP = chrome.runtime.getBackgroundPage;
+
+  });
