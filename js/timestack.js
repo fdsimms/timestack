c9087576db4d8bcaeac47b7a7ad8cb269af38474
Frankie <fdsimms@gmail.com>
2016-05-02 17:24:49 -0400

    Register timerStack directive in module and add script to html file.

--- js/timestack.js
+++ js/timestack.js
@@ -2,6 +2,7 @@ var timestack = angular.module("timestack", [
   "timestack.directives.timer",
   "timestack.directives.timerButtons",
   "timestack.directives.header",
+  "timestack.directives.timerStack",
   "timestack.directives.timerForm",
   "timestack.filters.formatTimer",
   "timestack.services.bgpService"
