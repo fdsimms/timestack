8d6bec5bfc2ec8d58d2343cb6b4e91780d2ecc65
Frankie Simms <fdsimms@gmail.com>
2019-03-05 12:12:35 -0500

    test: test circleci setup

---
+++ script.js
@@ -0,0 +1,2 @@
+const comment = require(__dirname + "/test-reporter/src/index.js").comment;
+comment('/test-output.json');
---
+++ test.js
@@ -0,0 +1,4 @@
+test('basic test', () => {
+  expect(2 + 3 + 6).toBe(12);
+});
+
